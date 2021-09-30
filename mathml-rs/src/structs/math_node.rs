575a366804f12b2f766149df023905d4b3ac8020
Pranav Ballaney <ballaneypranav@gmail.com>
2021-09-30 10:15:55 +0530

    Function to shift indices of all MathNodes

--- mathml-rs/src/structs/math_node.rs
+++ mathml-rs/src/structs/math_node.rs
@@ -34,6 +34,114 @@ impl MathNode {
             parent: None,
         })
     }
+    pub fn shift_indices(&mut self, shift: i32) -> &Self {
+        match self {
+            MathNode::Root(root) => {
+                for i in 0..root.children.len() {
+                    root.children[i] = ((root.children[i] as i32) + shift) as usize;
+                }
+                if let Some(parent) = root.parent {
+                    root.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Apply(apply) => {
+                for i in 0..apply.children.len() {
+                    apply.children[i] = ((apply.children[i] as i32) + shift) as usize;
+                }
+                if let Some(operator) = apply.operator {
+                    apply.operator = Some(((operator as i32) + shift) as usize);
+                }
+                for i in 0..apply.operands.len() {
+                    apply.operands[i] = ((apply.operands[i] as i32) + shift) as usize;
+                }
+                if let Some(parent) = apply.parent {
+                    apply.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Ci(ci) => {
+                if let Some(parent) = ci.parent {
+                    ci.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Cn(cn) => {
+                if let Some(parent) = cn.parent {
+                    cn.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Op(op) => {
+                if let Some(parent) = op.parent {
+                    op.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Constant(constant) => {
+                if let Some(parent) = constant.parent {
+                    constant.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Lambda(lambda) => {
+                for i in 0..lambda.children.len() {
+                    lambda.children[i] = ((lambda.children[i] as i32) + shift) as usize;
+                }
+                if let Some(expr) = lambda.expr {
+                    lambda.expr = Some(((expr as i32) + shift) as usize);
+                }
+                for i in 0..lambda.bindings.len() {
+                    lambda.bindings[i] = ((lambda.bindings[i] as i32) + shift) as usize;
+                }
+                if let Some(parent) = lambda.parent {
+                    lambda.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::BVar(bvar) => {
+                for i in 0..bvar.children.len() {
+                    bvar.children[i] = ((bvar.children[i] as i32) + shift) as usize;
+                }
+                if let Some(parent) = bvar.parent {
+                    bvar.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Piecewise(piecewise) => {
+                for i in 0..piecewise.children.len() {
+                    piecewise.children[i] = ((piecewise.children[i] as i32) + shift) as usize;
+                }
+                if let Some(otherwise) = piecewise.otherwise {
+                    piecewise.otherwise = Some(((otherwise as i32) + shift) as usize);
+                }
+                for i in 0..piecewise.pieces.len() {
+                    piecewise.pieces[i] = ((piecewise.pieces[i] as i32) + shift) as usize;
+                }
+                if let Some(parent) = piecewise.parent {
+                    piecewise.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Piece(piece) => {
+                for i in 0..piece.children.len() {
+                    piece.children[i] = ((piece.children[i] as i32) + shift) as usize;
+                }
+                if let Some(expr) = piece.expr {
+                    piece.expr = Some(((expr as i32) + shift) as usize);
+                }
+                if let Some(condition) = piece.condition {
+                    piece.condition = Some(((condition as i32) + shift) as usize);
+                }
+                if let Some(parent) = piece.parent {
+                    piece.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+            MathNode::Otherwise(otherwise) => {
+                for i in 0..otherwise.children.len() {
+                    otherwise.children[i] = ((otherwise.children[i] as i32) + shift) as usize;
+                }
+                if let Some(expr) = otherwise.expr {
+                    otherwise.expr = Some(((expr as i32) + shift) as usize);
+                }
+                if let Some(parent) = otherwise.parent {
+                    otherwise.parent = Some(((parent as i32) + shift) as usize);
+                }
+            }
+        }
+        self
+    }
 }
 
 impl Default for MathNode {
